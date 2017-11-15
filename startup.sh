b19ed4cf20863acd89a5f8ca17e438b57908835a
Nikolay Galkin <nikolay@exaltcg.com>
2017-11-15 12:18:58 +0200

    Remove hardcode

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-cd /Volumes/development/foxy/foxycli
-/Users/mozilla/.nvm/versions/node/v7.7.2/bin/node start.js
+
+. $HOME/.nvm/nvm.sh
+
+cd $(dirname "$0")
+$(nvm which 7) start.js
